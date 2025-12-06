12711df19bc7614dda3f54ccc9d4255f973d2b34
Ravi Kumar <rav7179@gmail.com>
2025-12-06 17:27:58 +0530

    changed

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 export default function handler(req, res) {
-  if (req.method == "GET") {
-    if (req.headers["X-Api-Key"] !== "my-secret-key")
-      res.status(200).json({ authorized: false });
-    else res.status(200).json({ authorized: true });
-  } else {
+  if (req.method !== "GET") {
     res.status(200).json({ message: "method not allowed" });
+  } else if (req.headers["X-Api-Key"] !== "my-secret-key") {
+    console.log("not");
+
+    res.status(200).json({ authorized: false });
+  } else {
+    console.log("yes");
+    res.status(200).json({ authorized: true });
   }
 }
