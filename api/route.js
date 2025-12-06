5f44a4eec5b0f85bea4d2d6502d0097d55f0295b
Ravi Kumar <rav7179@gmail.com>
2025-12-06 17:08:10 +0530

    method changed

--- api/route.js
+++ api/route.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+export default function GET(req, res) {
   if (req.headers["X-Api-Key"] !== "my-secret-key")
     res.status(200).json({ authorized: false });
   else res.status(200).json({ authorized: true });
